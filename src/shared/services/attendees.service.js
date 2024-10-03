bd35ed2630beed1156a50aa7fd6dd2d0e9ff3158
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-03 00:47:44 -0500

    fix: patch url request changed

--- src/shared/services/attendees.service.js
+++ src/shared/services/attendees.service.js
@@ -14,6 +14,7 @@ export class AttendeesService {
         const data = {
             checkedInAt: newCheckedIn
         }
-        return http.patch(`${this.resourceEndPoint}/${id}`, data)
+
+        return http.patch(`${this.resourceEndPoint}/${id}`, data);
     }
 }
